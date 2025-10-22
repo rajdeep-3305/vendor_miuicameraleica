0f2f1e1b2a12d52bfb091c4b0d661225f4f529e7
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2025-10-22 11:48:10 +0000

    Fix build conflicts

--- config.mk
+++ config.mk
@@ -1,24 +1,66 @@
-VENDOR_PATH := vendor/MiuiCameraLeica
-
-# --- Proprietary File Copying ---
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/vendor/lib64,system/vendor/lib64) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCameraLeica/lib/arm64,system/priv-app/MiuiCameraLeica/lib/arm64) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiExtraPhoto/lib/arm64,system/priv-app/MiuiExtraPhoto/lib/arm64) \
-    $(VENDOR_PATH)/init/init.leica.rc:system/etc/init/init.leica.rc \
-    $(VENDOR_PATH)/bin/leica_config_copy.sh:system/bin/leica_config_copy.sh \
-    $(VENDOR_PATH)/configs/ziyi_pro.json:system/etc/leica/ziyi_pro.json
-
-# --- Prebuilt Application Packages ---
 PRODUCT_PACKAGES += \
     MiuiCameraLeica \
     MiuiExtraPhoto \
-    MiuiGalleryEditor
+    MiuiGalleryEditor \
+    miuicamera-permissions.xml \
+    android.hardware.camera.flash-autofocus.xml \
+    android.hardware.camera.front.xml \
+    android.hardware.camera.full.xml \
+    android.hardware.camera.raw.xml \
+    privapp-permissions-miuicamera.xml \
+    miuicamera-hiddenapi-package-whitelist.xml \
+    init.leica.rc \
+    ziyi_pro.json \
+    leica_config_copy.sh \
+    libarcsoft_single_chart_calibration \
+    libijksdl \
+    libijkplayer \
+    libDocumentProcess \
+    libc++_shared_miuicam \
+    libgui_shim_miuicamera_app \
+    libsnpe_dsp_domains_v2 \
+    libsnpe_dsp_skel \
+    libsymphony-cpu \
+    libbarhopper_v3 \
+    libAIPOSE \
+    libyuv_leica_app \
+    libcamera_mianode_jni.xiaomi_app \
+    libYuvWatermark \
+    libsnpe_dsp_v66_domains_v2_skel \
+    libsnpe_dsp_v65_domains_v2_skel \
+    libion_miuicam \
+    libijkffmpeg \
+    libmicampostproc_client_app \
+    libcamera_algoup_jni.xiaomi_app \
+    libcamera_yuv_jni \
+    libmiuiblursdk_leica_app \
+    libc++_miuicam \
+    libjpeg_miuicam \
+    libmotion_photo_c++_shared \
+    libxcrash_dumper \
+    librefocus_mibokeh \
+    libselection \
+    librefocus \
+    libxcrash \
+    libyuv_extra_app \
+    libmotion_photo \
+    libwa_refocus_extraphoto \
+    libmiuiblursdk_extra_app \
+    libcamera_ispinterface_jni.xiaomi \
+    libgui_shim_miuicamera \
+    libmegvii_bokeh_jni \
+    libmorpho_Lowlight
 
-# --- System Property Files ---
-# This line includes our new, clean properties file.
-PRODUCT_PROPERTY_FILES += \
-    $(VENDOR_PATH)/leica.prop
+# --- System Property Overrides ---
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.lens.oem_camera_package=com.android.camera \
+    vendor.camera.aux.packagelist=com.android.camera \
+    persist.vendor.camera.privapp.list=com.android.camera \
+    ro.product.system.manufacturer=Xiaomi \
+    ro.product.manufacturer=Xiaomi \
+    ro.build.characteristics=default \
+    ro.control_privapp_permissions=log \
+    ro.product.mod_device=ziyi_pro_global \
+    ro.product.device=ziyi_pro \
+    ro.product.system.device=ziyi_pro
 
