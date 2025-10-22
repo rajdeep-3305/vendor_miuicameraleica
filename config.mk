6ff43288a8cda18a11d32cf6dd652a29a44ebec3
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2025-10-22 09:15:33 +0000

    Cleanup and fixes

--- config.mk
+++ config.mk
@@ -1,37 +1,24 @@
 VENDOR_PATH := vendor/MiuiCameraLeica
 
+# --- Proprietary File Copying ---
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCameraLeica,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCameraLeica) \
-    \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiExtraPhoto,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiExtraPhoto) \
-    \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiGalleryEditor,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiGalleryEditor) \
-    \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
-    \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
-    \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
-    \
-    $(VENDOR_PATH)/init/init.leica.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.leica.rc \
-    $(VENDOR_PATH)/bin/leica_config_copy.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/leica_config_copy.sh \
-    $(VENDOR_PATH)/configs/ziyi_pro.json:$(TARGET_COPY_OUT_SYSTEM)/etc/leica/ziyi_pro.json
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/vendor/lib64,system/vendor/lib64) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCameraLeica/lib/arm64,system/priv-app/MiuiCameraLeica/lib/arm64) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiExtraPhoto/lib/arm64,system/priv-app/MiuiExtraPhoto/lib/arm64) \
+    $(VENDOR_PATH)/init/init.leica.rc:system/etc/init/init.leica.rc \
+    $(VENDOR_PATH)/bin/leica_config_copy.sh:system/bin/leica_config_copy.sh \
+    $(VENDOR_PATH)/configs/ziyi_pro.json:system/etc/leica/ziyi_pro.json
 
+# --- Prebuilt Application Packages ---
 PRODUCT_PACKAGES += \
     MiuiCameraLeica \
     MiuiExtraPhoto \
     MiuiGalleryEditor
 
-# --- System Property Overrides ---
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.lens.oem_camera_package=com.android.camera \
-    vendor.camera.aux.packagelist=com.android.camera \
-    persist.vendor.camera.privapp.list=com.android.camera \
-    ro.product.system.manufacturer=Xiaomi \
-    ro.product.manufacturer=Xiaomi \
-    ro.build.characteristics=default \
-    ro.control_privapp_permissions=log \
-    ro.product.mod_device=ziyi_pro_global \
-    ro.product.device=ziyi_pro \
-    ro.product.system.device=ziyi_pro
+# --- System Property Files ---
+# This line includes our new, clean properties file.
+PRODUCT_PROPERTY_FILES += \
+    $(VENDOR_PATH)/leica.prop
 
